6bd8c67268aafda66bc1d15e45e2435a8376aca1
Win32Forth <win32forth@mac.com>
2026-05-30 17:01:52 -0500

    Initial Commit

---
+++ FPCForth/FPCForthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FPCForthApp.swift
+//  FPCForth
+//
+//  Created by Thomas Zimmer mini on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FPCForthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
